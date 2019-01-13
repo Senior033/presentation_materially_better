2a2f90f64e800e158aea6025ee7d645d0f3353fd
Matt Carroll <mattcarroll@google.com>
2019-01-12 21:22:57 -0800

    Setup main.dart to display empty screen for live-coding, start at master_screen.dart

---
+++ lib/master_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MasterScreen extends StatefulWidget {
+  @override
+  _MasterScreenState createState() => _MasterScreenState();
+}
+
+class _MasterScreenState extends State<MasterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purpleAccent,
+    );
+  }
+}
