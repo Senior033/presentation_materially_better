443e1a0c0334a33f23005c4c1e71bd122cdf5867
Matt Carroll <mattcarroll@google.com>
2019-01-12 21:55:35 -0800

    Step 3: Change theme accent color by tapping color palette circle.

--- lib/colors_screen.dart
+++ lib/colors_screen.dart
@@ -1,4 +1,5 @@
 import 'package:final_app/demo_content/colors.dart';
+import 'package:final_app/demo_tools/theme_switcher.dart';
 import 'package:flutter/material.dart';
 
 class ColorsScreen extends StatefulWidget {
@@ -36,22 +37,27 @@ class ColorGridButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: Colors.white,
-          width: 6.0,
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: const Color(0x22000000),
-            offset: Offset(0.0, 4.0),
-            blurRadius: 4.0,
+    return GestureDetector(
+      onTap: () {
+        ThemeSwitcher.of(context).accentColor = color;
+      },
+      child: Container(
+        margin: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.white,
+            width: 6.0,
           ),
-        ],
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0x22000000),
+              offset: Offset(0.0, 4.0),
+              blurRadius: 4.0,
+            ),
+          ],
+        ),
       ),
     );
   }
