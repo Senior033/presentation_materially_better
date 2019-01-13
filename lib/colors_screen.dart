5f416c2d5ecd141e5559f2b4a59ec79ad1052060
Matt Carroll <mattcarroll@google.com>
2019-01-12 21:45:34 -0800

    Create GridView with 3 example colored Containers.

--- lib/colors_screen.dart
+++ lib/colors_screen.dart
@@ -8,8 +8,26 @@ class ColorsScreen extends StatefulWidget {
 class _ColorsScreenState extends State<ColorsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Colors'),
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(8.0),
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+        ],
+      ),
     );
   }
 }
