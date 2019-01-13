8dbd5fbeccde1317823eba5ad904315f6548c79d
Matt Carroll <mattcarroll@google.com>
2019-01-12 22:08:51 -0800

    Step 5: Add drawer header image, but no color blend mode.

--- lib/theme_screen.dart
+++ lib/theme_screen.dart
@@ -1,3 +1,4 @@
+import 'package:final_app/demo_content/images.dart';
 import 'package:flutter/material.dart';
 
 class ThemeScreen extends StatefulWidget {
@@ -8,8 +9,21 @@ class ThemeScreen extends StatefulWidget {
 class _ThemeScreenState extends State<ThemeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Theme Screen'),
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: drawerArtLoadingHeight,
+            color: Theme.of(context).accentColor,
+            child: Image.network(
+              drawerArt,
+              height: drawerArtLoadingHeight,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
